3f7d580f69d4898a95414b4bf15f223f0c4b9d53
lingakrishnaveni57-ctrl <lingakrishnaveni@gmail.com>
2026-03-02 09:17:43 +0000

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function Home() {
+  const [message, setMessage] = useState("Loading...")
+
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(() => setMessage("Backend not reachable"))
+  }, [])
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          {message}
+        </h1>
+        <p className="mt-4 text-gray-600">
+          DevOps Assignment Frontend
+        </p>
+      </div>
+    </div>
+  )
+}
