f32c763b6273506f086e2bc9fc3981b415587955
lingakrishnaveni57-ctrl <lingakrishnaveni@gmail.com>
2026-03-03 12:09:41 +0000

    new commit

--- app/api.ts
+++ app/api.ts
@@ -4,10 +4,3 @@ export async function getUsers() {
   const res = await fetch(`${API_URL}/users/`);
   return res.json();
 }
-import { getUsers } from "./api";
-
-export default async function Page() {
-  const users = await getUsers();
-  return <pre>{JSON.stringify(users, null, 2)}</pre>;
-}
-
