37af25e59a53fef6d4a282e93841eb1fb1d83ceb
lingakrishnaveni57-ctrl <lingakrishnaveni@gmail.com>
2026-03-06 09:53:20 +0000

    git add app/api/backend/route.ts modified

--- app/api/backend/route.ts
+++ app/api/backend/route.ts
@@ -3,4 +3,13 @@ export async function GET() {
   const data = await response.json();
 
   return Response.json(data);
+export async function POST(req: Request) {
+  const body = await req.json();
+  const res = await fetch("http://13.201.22.139:8000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  return Response.json(await res.json());
 }
+
